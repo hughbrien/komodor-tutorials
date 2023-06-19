e368c054da548d33fcdd56d6ed83be00fba1f4ba
hughbrien <hugh@hughbrien.com>
2023-06-19 15:07:41 -0400

    Updated Label for use across multiple clusters

---
+++ run-tutorial.sh
@@ -0,0 +1,16 @@
+## Run this commands.
+## Create and Deploy a simple Pythong Application
+kubectl create ns komodor-tutorial
+kubectl apply -n komodor-tutorial -f https://raw.githubusercontent.com/hughbrien/komodor-tutorials/master/failure-scenarios/application-error-with-exception/simple-application.yaml
+
+## Now Lets Break it
+## Generate an Availability Evnet
+kubectl apply -n komodor-tutorial -f https://raw.githubusercontent.com/hughbrien/komodor-tutorials/master/failure-scenarios/application-error-with-exception/application-error.yaml
+
+
+## Delete Configuration
+kubectl delete -n komodor-tutorial -f https://raw.githubusercontent.com/hughbrien/komodor-tutorials/master/failure-scenarios/application-error-with-exception/simple-application.yaml
+kubectl delete ns komodor-tutorial
+
+
+
